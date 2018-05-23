ed8a0b8bf1c2e20e7c2873aeaece5cd8fa891928
Dyneteve <sound0020@gmail.com>
2018-05-23 09:55:15 -0400

    j5-common: Copy qcom mixer paths
    Change-Id: Id1441521a36eb8f3017c48f4556d817373eddb6e

--- product/audio.mk
+++ product/audio.mk
@@ -1,3 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+
+# Mixer
+PRODUCT_COPY_FILES += \
+	device/samsung/msm8916-common/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
