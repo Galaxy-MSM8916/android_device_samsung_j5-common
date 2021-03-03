8724b8a457b3c88ed2fbda0133f52ae56fd162bf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-04 00:08:48 +0100

    j5-common: libinit: Depend again on libinit_msm8916
    Change-Id: Ib000cb2356f0c1ad80b6185b121747e79b224f0e

--- init/init_j5.cpp
+++ init/init_j5.cpp
@@ -1,165 +1,20 @@
 /*
-   Copyright (c) 2017, The Linux Foundation. All rights reserved.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-	* Redistributions of source code must retain the above copyright
-	  notice, this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above
-	  copyright notice, this list of conditions and the following
-	  disclaimer in the documentation and/or other materials provided
-	  with the distribution.
-	* Neither the name of The Linux Foundation nor the names of its
-	  contributors may be used to endorse or promote products derived
-	  from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2017-2021, The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <android-base/file.h>
-#include <android-base/strings.h>
-
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-
-#define SIMSLOT_FILE "/proc/simslot_count"
-
-#include <android-base/properties.h>
-#include <android-base/logging.h>
-
-#include "vendor_init.h"
-#include "property_service.h"
-
-#define SERIAL_NUMBER_FILE "/efs/FactoryApp/serial_no"
-
-using android::base::GetProperty;
-using android::base::ReadFileToString;
-using android::base::Trim;
-
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-}
-
-/* Read the file at filename and returns the integer
- * value in the file.
- *
- * @prereq: Assumes that integer is non-negative.
- *
- * @return: integer value read if succesful, -1 otherwise. */
-int read_integer(const char* filename)
-{
-	int retval;
-	FILE * file;
-
-	/* open the file */
-	if (!(file = fopen(filename, "r"))) {
-		return -1;
-	}
-	/* read the value from the file */
-	fscanf(file, "%d", &retval);
-	fclose(file);
-
-	return retval;
-}
-
-void set_fingerprint()
-{
-	std::string fingerprint = GetProperty("ro.build.fingerprint", "");
-
-	if ((strlen(fingerprint.c_str()) > 1) && (strlen(fingerprint.c_str()) <= PROP_VALUE_MAX))
-		return;
-
-	char new_fingerprint[PROP_VALUE_MAX+1];
-
-	std::string build_id = GetProperty("ro.build.id","");
-	std::string build_tags = GetProperty("ro.build.tags","");
-	std::string build_type = GetProperty("ro.build.type","");
-	std::string device = GetProperty("ro.product.device","");
-	std::string incremental_version = GetProperty("ro.build.version.incremental","");
-	std::string release_version = GetProperty("ro.build.version.release","");
-
-	snprintf(new_fingerprint, PROP_VALUE_MAX, "samsung/%s/%s:%s/%s/%s:%s/%s",
-		device.c_str(), device.c_str(), release_version.c_str(), build_id.c_str(),
-		incremental_version.c_str(), build_type.c_str(), build_tags.c_str());
-
-	property_override_dual("ro.build.fingerprint", "ro.boot.fingerprint", new_fingerprint);
-}
-
-void set_dsds_properties()
-{
-	property_override("ro.multisim.simslotcount", "2");
-	property_override("ro.telephony.ril.config", "simactivation");
-	property_override("persist.radio.multisim.config", "dsds");
-	property_override("rild.libpath2", "/system/lib/libsec-ril-dsds.so");
-}
-
-void set_gsm_properties()
-{
-	property_override("telephony.lteOnCdmaDevice", "0");
-	property_override("ro.telephony.default_network", "9");
-}
-
-void set_lte_properties()
-{
-	property_override("persist.radio.lte_vrte_ltd", "1");
-	property_override("telephony.lteOnCdmaDevice", "0");
-	property_override("telephony.lteOnGsmDevice", "1");
-	property_override("ro.telephony.default_network", "10");
-}
-
-void set_target_properties(const char *device, const char *model)
-{
-	property_override_dual("ro.product.device", "ro.product.vendor.device", device);
-	property_override_dual("ro.product.model", "ro.product.vendor.model", model);
-
-	property_override("ro.ril.telephony.mqanelements", "6");
-
-	/* check and/or set fingerprint */
-	set_fingerprint();
-
-	/* check for multi-sim devices */
-
-	/* check if the simslot count file exists */
-	if (access(SIMSLOT_FILE, F_OK) == 0) {
-		int sim_count = read_integer(SIMSLOT_FILE);
-
-		/* set the dual sim props */
-		if (sim_count == 2)
-			set_dsds_properties();
-	}
-
-	char const *serial_number_file = SERIAL_NUMBER_FILE;
-	std::string serial_number;
-
-	if (ReadFileToString(serial_number_file, &serial_number)) {
-        	serial_number = Trim(serial_number);
-        	property_override("ro.serialno", serial_number.c_str());
-	}
-}
+#include <init_msm8916_base.h>
 
 void vendor_load_properties()
 {
